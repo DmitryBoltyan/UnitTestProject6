81682e83915797fbaa0cab3df83c7086bbc54484
Dmitry Boltyan <dmitry.boltyan@gmail.com>
2019-02-11 14:54:23 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject6")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1730fb86-7208-49f1-87bb-59bd257d2b97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject6
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Test]
+        public void InvalidPassword()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://accounts.google.com/";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("//*[@id=\"identifierId\"]")).SendKeys("automationt4@gmail.com");
+            driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).SendKeys("InvalidPassword");
+            driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            string actualError = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[2]/div[2]/div")).Text;
+            string expectedError = "Неверный пароль. Повторите попытку или нажмите на ссылку \"Забыли пароль?\", чтобы сбросить его.";
+
+            Assert.AreEqual(actualError, expectedError);
+
+            driver.Close();
+        }
+
+        [Test]
+        public void ValidPassword()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://accounts.google.com/";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("//*[@id=\"identifierId\"]")).SendKeys("automationt4@gmail.com");
+            driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).SendKeys("123automation");
+            driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            string actualError = driver.FindElement(By.XPath("//div[2]/c-wiz/div[1]/c-wiz/c-wiz/div/div[4]/div/div/header/h1")).Text;
+            string expectedError = "Добро пожаловать, Test Automation!";
+
+            Assert.AreEqual(actualError, expectedError);
+
+            driver.Quit();
+
+        }
+    }
+}
